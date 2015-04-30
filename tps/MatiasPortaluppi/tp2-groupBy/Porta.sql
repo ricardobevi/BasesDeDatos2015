2c15c2eecbe1491cea65d3bec89cd805f06916bb
matyporta <portaluppimatias@gmail.com>
2015-04-30 20:05:49 -0300

    Update Porta.sql

--- tps/MatiasPortaluppi/tp2-groupBy/Porta.sql
+++ tps/MatiasPortaluppi/tp2-groupBy/Porta.sql
@@ -1,7 +1,7 @@
 -- Trabajo Practico n2 - Matias Portaluppi
 
-SELECT nombre_cliente, precio*cantidad AS Total FROM VENTA GROUP BY nombre_cliente, precio*cantidad;
-SELECT fecha, precio*cantidad AS total FROM VENTA GROUP BY fecha, precio*cantidad;
-SELECT precio*cantidad AS total, fecha, id_producto FROM VENTA GROUP BY fecha, id_producto, precio*cantidad; 
-SELECT cantidad, nombre_cliente, fecha FROM VENTA GROUP BY nombre_cliente, fecha, cantidad; 
-SELECT cantidad, nombre_cliente FROM VENTA GROUP BY nombre_cliente, cantidad;
+SELECT nombre_cliente, sum(precio*cantidad) AS total FROM VENTA GROUP BY nombre_cliente;
+SELECT fecha, sum(precio*cantidad) AS total FROM VENTA GROUP BY fecha;
+SELECT sum(precio*cantidad) AS total, fecha, id_producto FROM VENTA GROUP BY fecha, id_producto; 
+SELECT sum(cantidad) as total, nombre_cliente, fecha FROM VENTA GROUP BY nombre_cliente, fecha; 
+SELECT sum(cantidad) as total, nombre_cliente FROM VENTA GROUP BY nombre_cliente;
