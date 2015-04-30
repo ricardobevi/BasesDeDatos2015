291fce5728fb84c06d3d2f3a40b4d93d0dcee387
matyporta <portaluppimatias@gmail.com>
2015-04-30 16:33:33 -0300

    Delete Porta.sql

--- tps/MatiasPortaluppi/tp2-groupBy/Porta.sql
+++
@@ -1,7 +0,0 @@
--- Trabajo Practico n2 - Matias Portaluppi
-
-SELECT nombre_cliente, precio*cantidad AS Total FROM VENTA GROUP BY nombre_cliente, precio*cantidad;
-SELECT fecha, precio*cantidad AS total FROM VENTA GROUP BY fecha, precio*cantidad;
-SELECT precio*cantidad AS total, fecha, id_producto FROM VENTA GROUP BY fecha, id_producto, precio*cantidad; 
-SELECT cantidad, nombre_cliente, fecha FROM VENTA GROUP BY nombre_cliente, fecha, cantidad; 
-SELECT cantidad, nombre_cliente FROM VENTA GROUP BY nombre_cliente, cantidad;
